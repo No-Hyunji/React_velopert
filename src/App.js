7806eac4c1933414d0db2819fd7c34ca1e883e7b
No-Hyunji <kiekuy47@gmail.com>
2021-02-08 14:56:51 +0900

    input

--- src/App.js
+++ src/App.js
@@ -1,21 +1,12 @@
+import React from "react";
 import Hello from "./Hello";
+import Wrapper from "./Wrapper";
+import Counter from "./Counter";
+import InputSample from "./InputSample";
 import "./App.css";
 
 function App() {
-  const name = "react";
-  const style = {
-    backgroundColor: "black",
-    color: "aqua",
-    fontSize: 24,
-    padding: "1rem",
-  };
-  return (
-    <>
-      <Hello name="react" color="red" />
-      <div style={style}>{name}</div>
-      <div className="gray-box"></div>
-    </>
-  );
+  return <InputSample />;
 }
 
 export default App;
