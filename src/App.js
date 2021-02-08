84f6c806c57e569504c52583bc5551dc109f1b15
No-Hyunji <kiekuy47@gmail.com>
2021-02-08 16:45:35 +0900

    ArrayRendering

--- src/App.js
+++ src/App.js
@@ -3,10 +3,11 @@ import Hello from "./Hello";
 import Wrapper from "./Wrapper";
 import Counter from "./Counter";
 import InputSample from "./InputSample";
+import UserList from "./UserList";
 import "./App.css";
 
 function App() {
-  return <InputSample />;
+  return <UserList />;
 }
 
 export default App;
