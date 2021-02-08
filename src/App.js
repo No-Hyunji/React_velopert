4e7d8cea277862399909f97a8a819d992f15e07e
No-Hyunji <kiekuy47@gmail.com>
2021-02-08 17:50:32 +0900

    useRef

--- src/App.js
+++ src/App.js
@@ -1,13 +1,31 @@
-import React from "react";
-import Hello from "./Hello";
-import Wrapper from "./Wrapper";
-import Counter from "./Counter";
-import InputSample from "./InputSample";
+import React, { useRef } from "react";
 import UserList from "./UserList";
 import "./App.css";
 
 function App() {
-  return <UserList />;
+  const users = [
+    {
+      id: 1,
+      username: "velopert",
+      email: "public,velopert@gmail.com",
+    },
+    {
+      id: 2,
+      username: "tester",
+      email: "tester@example,com",
+    },
+    {
+      id: 3,
+      username: "liz",
+      email: "liz@example.com",
+    },
+  ];
+
+  const nextId = useRef(4);
+  const onCreate = () => {
+    nextId.current += 1;
+  };
+  return <UserList users={users} />;
 }
 
 export default App;
